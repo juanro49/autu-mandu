65554607b088e691a674037dad6b00a0192b55ae
Jan Kuehle <jkuehle90@gmail.com>
2015-06-28 09:42:00 +0200

    Removed dependency on dragsortlistview. Replaced usage with RecyclerView and ItemTouchHelper.

--- app/src/main/java/me/kuehle/carreport/gui/PreferencesReportOrderFragment.java
+++ app/src/main/java/me/kuehle/carreport/gui/PreferencesReportOrderFragment.java
@@ -16,39 +16,29 @@
 
 package me.kuehle.carreport.gui;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-import com.mobeta.android.dslv.DragSortListView;
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import me.kuehle.carreport.Preferences;
 import me.kuehle.carreport.R;
 import me.kuehle.carreport.data.report.AbstractReport;
 
-public class PreferencesReportOrderFragment extends ListFragment {
-    private class ReportDropListener implements DragSortListView.DropListener {
-        @Override
-        public void drop(int from, int to) {
-            String item = adapter.getItem(from);
-            adapter.remove(item);
-            adapter.insert(item, to);
-            adapter.notifyDataSetChanged();
-
-            Preferences prefs = new Preferences(getActivity());
-            reportClasses.add(to, reportClasses.remove(from));
-            prefs.setReportOrder(reportClasses);
-        }
-    }
-
-    private ArrayAdapter<String> adapter;
-    private List<Class<? extends AbstractReport>> reportClasses;
+public class PreferencesReportOrderFragment extends Fragment {
+    private ItemTouchHelper mItemTouchHelper;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -57,23 +47,129 @@ public class PreferencesReportOrderFragment extends ListFragment {
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        DragSortListView lv = (DragSortListView) getListView();
-        lv.setDropListener(new ReportDropListener());
+        ReportOrderAdapter adapter = new ReportOrderAdapter();
 
-        adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_drag, android.R.id.text1);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(android.R.id.list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        Preferences prefs = new Preferences(getActivity());
-        reportClasses = prefs.getReportOrder();
-        for (Class<? extends AbstractReport> reportClass : reportClasses) {
-            AbstractReport report = AbstractReport.newInstance(reportClass, getActivity());
-            if (report != null) {
-                adapter.add(report.getTitle());
+        ItemTouchHelper.Callback callback = new ReportOrderItemTouchHelperCallback(adapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    private class ReportOrderViewHolder extends RecyclerView.ViewHolder implements View.OnTouchListener {
+        private TextView mTitle;
+
+        public ReportOrderViewHolder(View itemView) {
+            super(itemView);
+
+            mTitle = (TextView) itemView.findViewById(android.R.id.text1);
+            itemView.findViewById(R.id.drag_handle).setOnTouchListener(this);
+        }
+
+        public void bind(AbstractReport report) {
+            mTitle.setText(report.getTitle());
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                mItemTouchHelper.startDrag(this);
+            }
+
+            return false;
+        }
+    }
+
+    private class ReportOrderAdapter extends RecyclerView.Adapter<ReportOrderViewHolder> {
+        private Preferences mPrefs;
+        private List<AbstractReport> mReports;
+
+        public ReportOrderAdapter() {
+            mReports = new ArrayList<>();
+            mPrefs = new Preferences(getActivity());
+
+            List<Class<? extends AbstractReport>> reportClasses = mPrefs.getReportOrder();
+            for (Class<? extends AbstractReport> reportClass : reportClasses) {
+                AbstractReport report = AbstractReport.newInstance(reportClass, getActivity());
+                if (report != null) {
+                    mReports.add(report);
+                }
             }
         }
 
-        setListAdapter(adapter);
+        @Override
+        public ReportOrderViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_drag,
+                    parent, false);
+            return new ReportOrderViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ReportOrderViewHolder holder, int position) {
+            AbstractReport report = mReports.get(position);
+            holder.bind(report);
+        }
+
+        public void onItemMove(int fromPosition, int toPosition) {
+            Collections.swap(mReports, fromPosition, toPosition);
+            notifyItemMoved(fromPosition, toPosition);
+
+            saveReportOrder();
+        }
+
+        @Override
+        public int getItemCount() {
+            return mReports.size();
+        }
+
+        private void saveReportOrder() {
+            List<Class<? extends AbstractReport>> reportClasses = new ArrayList<>(mReports.size());
+            for (AbstractReport report : mReports) {
+                reportClasses.add(report.getClass());
+            }
+
+            mPrefs.setReportOrder(reportClasses);
+        }
+    }
+
+    private class ReportOrderItemTouchHelperCallback extends ItemTouchHelper.Callback {
+        private ReportOrderAdapter mAdapter;
+
+        public ReportOrderItemTouchHelperCallback(ReportOrderAdapter adapter) {
+            mAdapter = adapter;
+        }
+
+        @Override
+        public boolean isLongPressDragEnabled() {
+            return false;
+        }
+
+        @Override
+        public boolean isItemViewSwipeEnabled() {
+            return false;
+        }
+
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+            return makeMovementFlags(dragFlags, 0);
+        }
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder source,
+                              RecyclerView.ViewHolder target) {
+            mAdapter.onItemMove(source.getAdapterPosition(), target.getAdapterPosition());
+            return true;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int i) {
+        }
     }
 }
