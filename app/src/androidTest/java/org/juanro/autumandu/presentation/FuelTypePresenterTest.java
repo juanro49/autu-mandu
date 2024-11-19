49c6c5d6582386094358b2e0375c6bb6807c549e
juanro <juanro@juanro-alpha15>
2024-11-19 21:00:18 +0100

    Correcciones ftests

--- app/src/androidTest/java/org/juanro/autumandu/presentation/FuelTypePresenterTest.java
+++ app/src/androidTest/java/org/juanro/autumandu/presentation/FuelTypePresenterTest.java
@@ -3,6 +3,8 @@ package org.juanro.autumandu.presentation;
 import android.content.Context;
 
 import org.juanro.autumandu.model.AutuManduDatabase;
+import org.juanro.autumandu.model.dao.StationDAO;
+import org.juanro.autumandu.model.entity.Station;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,6 +32,8 @@ public class FuelTypePresenterTest {
     private AutuManduDatabase mDB;
     private long mFuelType1Id;
     private long mFuelType2Id;
+    private long mStation1Id;
+    private long mStation2Id;
     private long mFuelType3Id;
     private long mFuelType4Id;
     private long mCar1Id;
@@ -44,14 +48,18 @@ public class FuelTypePresenterTest {
         CarDAO carDAO = mDB.getCarDao();
         FuelTypeDAO ftDAO = mDB.getFuelTypeDao();
         RefuelingDAO refDAO = mDB.getRefuelingDao();
+        StationDAO stDAO = mDB.getStationDao();
 
         FuelType ft1 = new FuelType("NoPB 95 - "+PECULIARITY, "Gasoline-"+PECULIARITY);
         FuelType ft2 = new FuelType("NoPB 98 - "+PECULIARITY, "Gasoline-"+PECULIARITY);
         FuelType ft3 = new FuelType("Diesel - "+PECULIARITY, "Diesel-"+PECULIARITY);
         FuelType ft4 = new FuelType("Gas - "+PECULIARITY, "Diesel-"+PECULIARITY);
 
-        Car car1 = new Car("Gasoline Car - "+PECULIARITY, 0, 0, null);
-        Car car2 = new Car("Mixed Car - "+PECULIARITY, 0, 0, null);
+        Station st1 = new Station("Iberdoex");
+        Station st2 = new Station("Campoex");
+
+        Car car1 = new Car("Gasoline Car - "+PECULIARITY, 0, 0, null, 0);
+        Car car2 = new Car("Mixed Car - "+PECULIARITY, 0, 0, null,0);
 
         long[] ftIds = ftDAO.insert(ft1, ft2, ft3, ft4);
         mFuelType1Id = ftIds[0];
@@ -59,16 +67,20 @@ public class FuelTypePresenterTest {
         mFuelType3Id = ftIds[2];
         mFuelType4Id = ftIds[3];
 
+        long[] stIds = stDAO.insert(st1, st2);
+        mStation1Id = stIds[1];
+        mStation2Id = stIds[2];
+
         long[] carIds = carDAO.insert(car1, car2);
         mCar1Id = carIds[0];
         mCar2Id = carIds[1];
 
-        Refueling ref1 = new Refueling(mCar1Id, mFuelType1Id, new Date(), 55, 4, 5.2f, false, "ref1-"+PECULIARITY);
-        Refueling ref2 = new Refueling(mCar1Id, mFuelType2Id, new Date(), 121, 4, 5.8f, false, "ref2-"+PECULIARITY);
-        Refueling ref3 = new Refueling(mCar1Id, mFuelType1Id, new Date(), 180, 6, 9.45f, false, "ref3-"+PECULIARITY);
-        Refueling ref4 = new Refueling(mCar2Id, mFuelType3Id, new Date(), 68, 4, 5.2f, false, "ref4-"+PECULIARITY);
-        Refueling ref5 = new Refueling(mCar2Id, mFuelType3Id, new Date(), 110, 4.2f, 4.6f, false, "ref5"+PECULIARITY);
-        Refueling ref6 = new Refueling(mCar2Id, mFuelType2Id, new Date(), 207, 6, 9.45f, false, "ref6-"+PECULIARITY);
+        Refueling ref1 = new Refueling(mCar1Id, mFuelType1Id, mStation1Id, new Date(), 55, 4, 5.2f, false, "ref1-"+PECULIARITY);
+        Refueling ref2 = new Refueling(mCar1Id, mFuelType2Id, mStation2Id, new Date(), 121, 4, 5.8f, false, "ref2-"+PECULIARITY);
+        Refueling ref3 = new Refueling(mCar1Id, mFuelType1Id, mStation1Id, new Date(), 180, 6, 9.45f, false, "ref3-"+PECULIARITY);
+        Refueling ref4 = new Refueling(mCar2Id, mFuelType3Id, mStation2Id, new Date(), 68, 4, 5.2f, false, "ref4-"+PECULIARITY);
+        Refueling ref5 = new Refueling(mCar2Id, mFuelType3Id, mStation1Id, new Date(), 110, 4.2f, 4.6f, false, "ref5"+PECULIARITY);
+        Refueling ref6 = new Refueling(mCar2Id, mFuelType2Id, mStation2Id, new Date(), 207, 6, 9.45f, false, "ref6-"+PECULIARITY);
         refDAO.insert(ref1, ref2, ref3, ref4, ref5, ref6);
     }
 
@@ -121,8 +133,8 @@ public class FuelTypePresenterTest {
 
         // following should have no effect on car1 but on car2 (also should not cache.
         mDB.getRefuelingDao().insert(
-                new Refueling(mCar2Id, mFuelType2Id, new Date(), 280, 7, 8.9f, false, "ref7-"+PECULIARITY),
-                new Refueling(mCar2Id, mFuelType2Id, new Date(), 355, 6.8f, 8.2f, false, "ref8-"+PECULIARITY));
+                new Refueling(mCar2Id, mFuelType2Id, mStation1Id, new Date(), 280, 7, 8.9f, false, "ref7-"+PECULIARITY),
+                new Refueling(mCar2Id, mFuelType2Id, mStation1Id, new Date(), 355, 6.8f, 8.2f, false, "ref8-"+PECULIARITY));
         assertEquals(mFuelType1Id, ft.getMostUsedId(mCar1Id));
         assertEquals(mFuelType2Id, ft.getMostUsedId(mCar2Id));
     }
@@ -138,7 +150,7 @@ public class FuelTypePresenterTest {
 
         // folowing should make Fuel Type 4 be used.
         mDB.getRefuelingDao().insert(
-                new Refueling(mCar2Id, mFuelType4Id, new Date(), 240, 4, 3.2f, false, "ref7-"+PECULIARITY));
+                new Refueling(mCar2Id, mFuelType4Id, mStation1Id, new Date(), 240, 4, 3.2f, false, "ref7-"+PECULIARITY));
         assertTrue(ft.isUsed(mFuelType4Id));
     }
 }
